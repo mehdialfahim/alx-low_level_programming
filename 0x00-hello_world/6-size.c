ef78670866f41f522e6180f3394985aed33b4965
byname <you@gmail.com>
2023-02-18 12:21:25 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
  */
 int main(void) 
 {
-printf("Size of char: %i byte(s)\n", sizeof(char));
-printf("Size of short: %i byte(s)\n", sizeof(short));
-printf("Size of int: %i byte(s)\n", sizeof(int));
-printf("Size of long: %i byte(s)\n", sizeof(long));
-printf("Size of long: %i byte(s)\n", sizeof(long int));
-printf("Size of long long: %i byte(s)\n", sizeof(long long));
-printf("Size of long long: %i byte(s)\n", sizeof(long long int));
-printf("Size of float: %i byte(s)\n", sizeof(float));
-printf("Size of double: %i byte(s)\n", sizeof(double));
-printf("Size of long double: %li byte(s)\n", sizeof(long double));
+printf("Size_of_char: %i_byte(s)\n", sizeof(char));
+printf("Size_of_short: %i_byte(s)\n", sizeof(short));
+printf("Size_of_int: %i_byte(s)\n", sizeof(int));
+printf("Size_of_long: %i_byte(s)\n", sizeof(long));
+printf("Size_of_long: %i_byte(s)\n", sizeof(long int));
+printf("Size_of_long_long: %i_byte(s)\n", sizeof(long long));
+printf("Size_of_long_long: %i_byte(s)\n", sizeof(long long int));
+printf("Size_of_float: %i_byte(s)\n", sizeof(float));
+printf("Size_of_double: %i_byte(s)\n", sizeof(double));
+printf("Size_of_long_double: %li_byte(s)\n", sizeof(long double));
 return (0);
 }
