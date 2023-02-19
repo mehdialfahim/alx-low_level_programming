14c6b40c9d21bbc2f6e5b527b4879ad981a5fbea
byname <you@gmail.com>
2023-02-19 08:40:56 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- * main - Entry 
- * Return: Always 0
- */
-int main(void) 
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
-printf("Size_of_char: %i_byte(s)\n", sizeof(char));
-printf("Size_of_short: %i_byte(s)\n", sizeof(short));
-printf("Size_of_int: %i_byte(s)\n", sizeof(int));
-printf("Size_of_long: %i_byte(s)\n", sizeof(long));
-printf("Size_of_long: %i_byte(s)\n", sizeof(long int));
-printf("Size_of_long_long: %i_byte(s)\n", sizeof(long long));
-printf("Size_of_long_long: %i_byte(s)\n", sizeof(long long int));
-printf("Size_of_float: %i_byte(s)\n", sizeof(float));
-printf("Size_of_double: %i_byte(s)\n", sizeof(double));
-printf("Size_of_long_double: %li_byte(s)\n", sizeof(long double));
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
+
