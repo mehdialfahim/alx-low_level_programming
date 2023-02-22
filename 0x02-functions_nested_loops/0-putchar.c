4a91a76db55c6161b8dd6f2c2770dd5a2925f8b2
byname <you@gmail.com>
2023-02-22 15:39:19 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
+#include <main.h>
 /**
  * main - Write a program that prints _putchar
  *
@@ -9,6 +6,14 @@
  */
 int main(void)
 {
-   printf("_putchar");
-   return 0;
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
