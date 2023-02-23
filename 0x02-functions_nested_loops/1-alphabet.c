35f98813eee143afa2edfa3653a189a875ba1c01
byname <you@gmail.com>
2023-02-22 16:12:50 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include “main.h”
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+int main(void)
+{
+_putchar(‘a’);
+_putchar(‘b’);
+_putchar(‘c’);
+_putchar(‘d’);
+_putchar(‘e’);
+_putchar(‘f’);
+_putchar(‘g’);
+_putchar(‘h’);
+_putchar(‘I’);
+_putchar(‘j’);
+_putchar(‘k’);
+_putchar(‘l’);
+_putchar(‘m’);
+_putchar(‘n’);
+_putchar(‘o’);
+_putchar(‘p’);
+_putchar(‘q’);
+_putchar(‘r’);
+_putchar(‘s’);
+_putchar(‘t’);
+_putchar(‘u’);
+_putchar(‘v’);
+_putchar(‘w’);
+_putchar(‘x’);
+_putchar(‘y’);
+_putchar(‘z’);
+_putchar('\n');
+return (0);
+}
